a0bdd07b73c71afb59d1bf40c2d4a974ac5fc698
Dmytro733 <ddp0496@gmail.com>
2023-01-12 17:53:01 +0200

    update to 'webpack v5'

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+import './js/ToDo.js';
+import './sass/style.sass';
+import 'bootstrap';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+let path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = !isProduction;
+
+let config = {
+  entry: './src/index.js',
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({filename: '[name].css'}),
+    new HtmlWebpackPlugin({ template: './index.html' })
+  ]
+}
+
+module.exports = (env, options) => {
+  
+  config.devtool = isProduction ? false : 'eval-cheap-module-source-map';
+  config.target = isProduction ? 'browserslist' : 'web'; 
+
+  return config;
+};
